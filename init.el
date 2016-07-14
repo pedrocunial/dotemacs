26a0cf11395f8134665b48255b18f4256b89f98a
pedrocunial <pedrocc4@al.insper.edu.br>
2016-07-14 17:06:28 -0300

    auto complete + line numbers

--- init.el
+++ init.el
@@ -15,7 +15,8 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(custom-enabled-themes (quote (tango-dark)))
- '(inhibit-startup-screen t))
+ '(inhibit-startup-screen t)
+ '(show-paren-mode t))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -24,3 +25,5 @@
  )
 
 (ac-config-default)
+
+(global-linum-mode)
