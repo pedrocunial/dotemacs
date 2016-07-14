11abdff118178b2307d0404fc1e27296bb00ca85
pedrocunial <pedrocc4@al.insper.edu.br>
2016-07-14 16:37:35 -0300

    Added the files itself

---
+++ elpa/popup-20160709.729/popup-autoloads.el
@@ -0,0 +1,18 @@
+;;; popup-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+
+;;;### (autoloads nil nil ("popup-pkg.el" "popup.el") (22407 59489
+;;;;;;  119059 224000))
+
+;;;***
+
+(provide 'popup-autoloads)
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; popup-autoloads.el ends here
