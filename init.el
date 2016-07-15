22ddda2fd215e9ad7fed4f012627b6f8f22df009
pedrocunial <pedrocc4@al.insper.edu.br>
2016-07-15 09:38:39 -0300

    nerd commenter, and new gitignore

--- init.el
+++ init.el
@@ -20,6 +20,7 @@
 
  ;; from http://stackoverflow.com/questions/23792526/indentation-inside-brackets-and-not-beside-closing-bracket-in-emacs
  '(c-hanging-braces-alist (quote ((substatement-open . (after)))))
+
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -38,3 +39,9 @@
 (require 'nodejs-repl)
 
 (load-theme 'suscolors t)
+
+;; (add-hook 'c-mode-common-hook '(lambda () (c-toggle-auto-state 1)))
+
+;; evil-nerd-commenter: Use , to comment a line
+;; (you can also do things like ",i(" also)
+(evilnc-default-hotkeys)
