f2dd0f2ba8c78c36f54d869cdea2e80e32a6ee2e
Pedro Cunial <pedro_cunial@hotmail.com>
2016-07-17 19:33:24 -0300

    updated description on init.el

--- init.el
+++ init.el
@@ -74,4 +74,5 @@ Return the absolute value of OFFSET, converted to string."
 
 ;; semantic mode, i don't quite get the use of this, soo i'm declaring it
 ;; as initially off, but i may change my mind, soo keeping this here
+;; UPDATE: Looks like I was a useful plugin after-all, I might give it a try
 (setq semantic-mode t)
