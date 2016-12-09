f4cb4314ece1756b750396bb978bc1ce30fb6d1a
Gil <Gil>
2016-12-08 22:59:56 -0200

    windows version of my spacemacs config

--- elpa/stan-mode-20160626.1518/stan-mode-pkg.el
+++
@@ -1,5 +0,0 @@
-(define-package "stan-mode" "20160626.1518" "Major mode for editing Stan files" 'nil :url "http://github.com/stan-dev/stan-mode" :keywords
-  '("languanges"))
-;; Local Variables:
-;; no-byte-compile: t
-;; End:
