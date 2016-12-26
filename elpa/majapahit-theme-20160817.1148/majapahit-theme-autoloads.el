a529ce748137a0c5d5fa8b9f4ede87e26845c45c
Pedro Cunial <pedrocunial@gmail.com>
2016-12-26 17:06:21 -0200

    Quick commit - Mon Dec 26 17:06:21 BRST 2016

--- elpa/majapahit-theme-20160817.1148/majapahit-theme-autoloads.el
+++
@@ -1,24 +0,0 @@
-;;; majapahit-theme-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
-
-;;;### (autoloads nil "majapahit-common" "majapahit-common.el" (22602
-;;;;;;  47269 127435 754000))
-;;; Generated autoloads from majapahit-common.el
-
-(when load-file-name (add-to-list 'custom-theme-load-path (file-name-as-directory (file-name-directory load-file-name))))
-
-;;;***
-
-;;;### (autoloads nil nil ("majapahit-dark-theme.el" "majapahit-light-theme.el"
-;;;;;;  "majapahit-theme-pkg.el") (22602 47269 210767 18000))
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; majapahit-theme-autoloads.el ends here
