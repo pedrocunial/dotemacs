ff440a66ad6d429673a6ba77dd0bb01fb31b622b
Pedro Cunial <pedrocc4@al.insper.edu.br>
2017-01-25 16:56:14 -0200

    crystal

---
+++ elpa/crystal-mode-20161008.1405/crystal-mode-pkg.el
@@ -0,0 +1,6 @@
+(define-package "crystal-mode" "20161008.1405" "Major mode for editing Crystal files" 'nil :keywords
+  '("languages" "crystal")
+  :url "https://github.com/jpellerin/emacs-crystal-mode")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
