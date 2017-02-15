20e60f45c4d954937015161471d8249cb153f70b
Pedro Cunial <pedrocunial@gmail.com>
2017-02-15 16:12:31 -0200

    merge done

--- elpa/crystal-mode-20161008.1405/crystal-mode-pkg.el
+++
@@ -1,6 +0,0 @@
-(define-package "crystal-mode" "20161008.1405" "Major mode for editing Crystal files" 'nil :keywords
-  '("languages" "crystal")
-  :url "https://github.com/jpellerin/emacs-crystal-mode")
-;; Local Variables:
-;; no-byte-compile: t
-;; End:
